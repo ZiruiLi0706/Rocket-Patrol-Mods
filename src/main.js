d4f785dfdecbc29cb71d14e5160623045eeef852
Zirui Li <zli397@ucsc.edu>
2021-04-23 11:15:41 -0700

    last

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ let keyLEFT, keyRIGHT, keyF, keyR;
 
 //Add your own (copyright-free) background music to the Play scene.
 //Allow the player to control the Rocket after it's fired, character in my case.
+//Used new typography on title screen.
+//Create a new scrolling tile sprite for the background.
 //Create new artwork for all of the in-game assets (rocket, spaceships, explosion).
 //Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi). 
-//
